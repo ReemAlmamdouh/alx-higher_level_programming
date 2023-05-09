f1b0d0f0df97a1ee1c78960c70e4ee8183ade578
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-05-09 08:23:37 -0700

    alpha

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
-2-print_alphabet.py
-for character in range(97, 123):
-    print("{}" .format(chr(character)), end="")
+#!/usr/bin/python3
+# 2-print_alphabet.py
+
+"""Print the alphabet in lowercase, not followed by a new line."""
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
