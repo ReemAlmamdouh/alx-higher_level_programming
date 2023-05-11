ad3cf810cc123d2c7157f92a0584176d402819a7
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-05-11 05:47:59 -0700

    inf

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    sum = 0
+    result = 0
     for arg in sys.argv:
         if arg != sys.argv[0]:
-            sum = sum + int(arg)
-            print(sum)
+            result += int(arg)
+    print(result)
