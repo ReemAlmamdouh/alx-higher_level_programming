74f03df2cd2effe31f08577ceed866d006460d50
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-06-07 02:01:50 -0700

    update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+# 3-say_my_name.py
+"""Defines a name-printing function."""
+
+
 def say_my_name(first_name, last_name=""):
-     """
-    Prints "My name is <first_name> <last_name>". If last_name is not provided, only prints "My name is <first_name>".
+    """Print a name.
 
     Args:
-    first_name (str): The first name to include in the message.
-    last_name (str): The last name to include in the message.
-
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
     Raises:
-    TypeError: If either first_name or last_name is not a string.
-
+        TypeError: If either of first_name or last_name are not strings.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
+# 4-print_square.py
+"""Defines a square-printing function."""
+
+
 def print_square(size):
-    """
-    Prints a square of the given size using the character '#'.
+    """Print a square with the # character.
 
     Args:
-    size (int): The length of each side of the square.
-
+        size (int): The height/width of the square.
     Raises:
-    TypeError: If size is not an integer.
-    ValueError: If size is less than 0.
-    TypeError: If size is a float and is less than 0.
-
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
+
     for i in range(size):
-        print("#" * size)
+        [print("#", end="") for j in range(size)]
+        print("")
