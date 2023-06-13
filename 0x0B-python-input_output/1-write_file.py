b6b9018c654be5bd7213d3a08a1ba154fb2ba416
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-06-13 05:48:39 -0700

    update

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Function that writes a string to a text file"""
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file
+    and returns number of charecters 
+    written"""
+
+    with open(filename, mode="w", encoding="utf-8") as f:
+        n_characters = f.write(text)
+
+    return n_characters
