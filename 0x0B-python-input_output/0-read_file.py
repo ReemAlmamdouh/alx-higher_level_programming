20a4abc7a25d430ad738b0c223d45db53498834c
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-06-13 04:50:16 -0700

    update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that reads a file"""
+
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout"""
+
+    with open(filename, encoding="utf-8") as f:
+        contents = f.read()
+        print(contents)
