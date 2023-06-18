5d0d9ceb69adcd1c49cc2663701b6f3f0c21e5d8
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-06-18 10:04:32 -0700

    update

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Defines a base model class."""
+import json
+import csv
+import turtle
+
+class Base:
+    """ Class Base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initializes instances """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects    
